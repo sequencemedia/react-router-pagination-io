cd33b47f84737280efd8abf72ad9fd0ee185c8b9
Tertychnyi <rett8716@gmail.com>
2022-10-04 20:32:07 +0300

    changed api

--- client/api/paginated-page/index.js
+++ client/api/paginated-page/index.js
@@ -1,3 +1,7 @@
 import fetch from 'isomorphic-fetch'
 
-export const paginatedPage = ({ page }) => fetch(`/api/${page}`).then((response) => response.json())
+// export const paginatedPage = ({ page }) => fetch(`/api/${page}`).then((response) => response.json())
+
+const API_URL = 'https://reqres.in/api/';
+
+export const paginatedPage = (page) => {return fetch(`${API_URL}users?page=${page}`).then(res => res.json()).catch((e) => (console.log(e)));};
--- client/app/components/paginated-page/component.js
+++ client/app/components/paginated-page/component.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useEffect , useState} from 'react'
 import PropTypes from 'prop-types'
 
 import {
@@ -6,21 +6,36 @@ import {
 } from 'react-router-dom'
 
 import Pagination from 'react-router-pagination-io/client/app/components/common/pagination'
+import { paginatedPage } from '../../../api/paginated-page'
 
-const PaginatedPage = ({ pageNumber }) => (
-  <section>
-    <h1>Pagination (Page {pageNumber})</h1>
-    <Pagination />
-    <nav>
-      <p>Return to the <Link to='/'>index page</Link>.</p>
-      {do {
-        if (pageNumber) {
-          <p>Redux has state for page {pageNumber}.</p>
-        }
-      }}
-    </nav>
-  </section>
-)
+
+const PaginatedPage = ({ pageNumber }) => {
+  const [userInfo , setUserInfo] = useState([])
+  const fetchData = async () => {
+    const {data} = await paginatedPage(pageNumber);
+    setUserInfo(data);
+  };
+
+
+  useEffect(() => {
+    fetchData();
+  }, [pageNumber]);
+  return (
+    <section>
+      <h1>Pagination (Page {pageNumber})</h1>
+      <Pagination/>
+      <nav>
+        <p>Return to the <Link to='/'>index page</Link>.</p>
+        {do {
+          if (pageNumber) {
+            <p>Redux has state for page {pageNumber}.</p>
+          }
+        }}
+        {userInfo[0] && userInfo.map(item=><li>{item.email}</li>)}
+      </nav>
+    </section>
+  )
+}
 
 PaginatedPage.propTypes = {
   pageNumber: PropTypes.number.isRequired
